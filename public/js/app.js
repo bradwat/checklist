f2bc24fec2e5da9172a4eabf3f63750b64a7722c
bradwat <bradwat@live.com>
2016-03-04 00:36:02 -0500

    doggy checklist

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,4 @@
-var app = angular.module('ngChecklist',
-[]);
-app.controller('ChecklistController',
-function($scope){
-  $scope.checkList = ['rake the yard',
-                      'take a nap',
-                      'take another nap',
-                      'walk the dog',
-                      'eat that leftover meatloaf']
 $scope.savehecklist = function() {
-    var ChecklistInfo = $scope.currentTodo;
-    $scope.checkList.push(todoInfo);
+    var ChecklistInfo = $scope.currentChecklist;
+    $scope.checkList.push(checklistInfo);
     $scope.currentChecklist = "";
-}
-
-$scope.removeChecklist = function(singleTodo){
-  var position = $scope.checkList.indexOf(singleTodo);
-  $scope.checkList.splice(position,1);
-}
-
-$scope.editChecklist = function(singleChecklist){
-  $scope.currentChecklist = singleChecklist;
-  $scope.removeChecklist(singleChecklist);
-}
-
-$scope.moveUp = function(singleChecklist){
-  var index = $scope.todoList.indexOf(singleChecklist);
-  $scope.todoList.splice(index,1);
-  $scope.todoList.splice(index-1, 0, singleChecklist);
-
-}
-});
