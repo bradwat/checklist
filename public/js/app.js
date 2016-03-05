443b6518d323a3959ca7fb1c41472d2db24ae3e0
bradwat <bradwat@live.com>
2016-03-04 23:13:51 -0500

    angularized

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,40 @@
-$scope.savehecklist = function() {
-    var ChecklistInfo = $scope.currentChecklist;
-    $scope.checkList.push(checklistInfo);
-    $scope.currentChecklist = "";
+var app = angular.module('dogCheckList', []);
+ +
+ +app.controller('ChecklistController', function($scope){
+ +
+ +  $scope.dogs = [
+ +    {
+ +      name: 'T.J.',
+ +      gotMeds: false
+ +    },
+ +    {
+ +      name: 'Lola',
+ +      gotMeds: false
+ +    },
+ +    {
+ +      name: 'Gypsy',
+ +      gotMeds: false
+ +    },
+ +    {
+ +      name: 'Lucy',
+ +      gotMeds: false
+ +    },
+ +    {
+ +      name: 'Sophie',
+ +      gotMeds: false
+ +    },
+ +  ];
+ +
+ +  $scope.addDogToMedsList = function(){
+ +    $scope.dog.gotMeds = false;
+ +    $scope.dogs.push($scope.dog);
+ +    $scope.dog = {};
+ +  }
+ +  // $scope.savechecklist = function() {
+ +  //     var ChecklistInfo = $scope.currentChecklist;
+ +  //     $scope.checkList.push(checklistInfo);
+ +  //     $scope.currentChecklist = "";
+ +  //   }
+ +
+ +
+ +});
